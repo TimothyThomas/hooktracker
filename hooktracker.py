5d8fe8c0c611a81c6f99ef4be9780302db934092
Tim Thomas <timrthomas2@gmail.com>
2020-12-01 17:22:37 -0500

    Add settings window.

--- hooktracker.py
+++ hooktracker.py
@@ -1,3 +1,4 @@
+import json
 import socket
 import logging
 import time
@@ -9,12 +10,75 @@ from pathlib import Path
 import PySimpleGUIQt as sg
 import tailer
 
-DASHBOARD_UDP_PORT = 49100
-COORDINATE_PRECISION = 1    # number of digits after decimal
-UPDATE_FREQUENCY = 500      # time in milliseconds
-COORDINATE_UNITS = 'm'  #  feet.inches, inches, mm, m
-LOG_FILE_DIRECTORY = Path('C:\Marvelmind\dashboard\logs')
-HEDGE_ADDRS = [36, 38]  # list of addresses to be tracked. 
+SETTINGS_FILE = Path(Path(__file__).parent, 'settings.cfg')
+DEFAULT_SETTINGS = {'precision': 1,
+                    'update_freq': 500, 
+                    'units': 'feet.inches',
+                    'log_dir': str(Path('C:\Marvelmind\dashboard\logs')),
+                    'hedge_addrs': '36,38',
+                    'allowed_system_vs_log_time_delta': 1000,
+                    'color_theme': 'DarkBlue13', 
+                    }
+SETTINGS_KEYS_TO_ELEMENT_KEYS = {
+                    'precision': '-PREC-',
+                    'update_freq': '-FREQ-', 
+                    'units': '-UNITS-',
+                    'log_dir': '-LOGDIR-',
+                    'hedge_addrs': '-ADDR-',
+                    'allowed_system_vs_log_time_delta': '-ALLOW_DELTA_T-',
+                    'color_theme': '-THEME-',
+                    }
+
+UNITS_CHOICES = ['m', 'mm', 'inches', 'feet.inches']
+PREC_CHOICES = [0,1,2]
+
+
+def load_settings(settings_file, default_settings):
+    try:
+        with open(settings_file, 'r') as f:
+            settings = json.load(f)
+    except Exception as e:
+        settings = default_settings
+        save_settings(settings_file, settings, None)
+    return settings
+
+
+def save_settings(settings_file, settings, values):
+    if values:
+        for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:  # update window with the values read from settings file
+            try:
+                settings[key] = values[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]]
+            except Exception as e:
+                print(f'Problem updating settings from window values. Key = {key}')
+
+    with open(settings_file, 'w') as f:
+        json.dump(settings, f)
+
+
+def create_settings_window(settings):
+    sg.theme(settings['color_theme'])
+
+    def TextLabel(text): return sg.Text(text+':', justification='r', size=(25,1))
+
+    layout = [  [sg.Text('Settings', font=('Work Sans', 12))],
+                [TextLabel('Track Hedge Address(es)'),sg.Input(key='-ADDR-')],
+                [TextLabel('Units'), sg.Combo(UNITS_CHOICES, key='-UNITS-')],
+                [TextLabel('Precision'), sg.Combo(PREC_CHOICES, key='-PREC-')],
+                [TextLabel('Refresh Rate (ms)'), sg.Input(key='-FREQ-')],
+                [TextLabel('Logfile Folder'),sg.Input(key='-LOGDIR-'), sg.FolderBrowse(target='-LOGDIR-')],
+                [TextLabel('Time until log considered stale (ms)'), sg.Input(key='-ALLOW_DELTA_T-')],
+                [TextLabel('Color Theme'),sg.Combo(sg.theme_list(), key='-THEME-')],
+                [sg.Button('Save'), sg.Button('Restore Defaults'), sg.Button('Exit')]  ]
+
+    window = sg.Window('Settings', layout, keep_on_top=True, finalize=True)
+
+    for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:   # update window with the values read from settings file
+        try:
+            window[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]].update(value=settings[key])
+        except Exception as e:
+            print(f'Problem updating PySimpleGUI window from settings. Key = {key}')
+
+    return window
 
 
 def connect_to_udp_stream():
@@ -46,41 +110,13 @@ def get_hedge_pos_udp():
     y_mm = int.from_bytes(data[13:17], byteorder='little', signed=True)
     z_mm = int.from_bytes(data[17:21], byteorder='little', signed=True)
 
-    fmt = f'.{COORDINATE_PRECISION}f'
-    if COORDINATE_UNITS == 'feet.inches':
-        x = f"""{x_mm / 25.4 // 12}\' {x_mm / 25.4 % 12:{fmt}}\""""
-        y = f"""{y_mm / 25.4 // 12}\' {y_mm / 25.4 % 12:{fmt}}\""""
-        z = f"""{z_mm / 25.4 // 12}\' {z_mm / 25.4 % 12:{fmt}}\""""
-    
-    elif COORDINATE_UNITS == 'inches':
-        x = f'{x_mm / 25.4:{fmt}}'
-        y = f'{y_mm / 25.4:{fmt}}'
-        z = f'{z_mm / 25.4:{fmt}}'
-    
-    elif COORDINATE_UNITS == 'mm':
-        x = f'{x_mm:{fmt}}'
-        y = f'{y_mm:{fmt}}'
-        z = f'{z_mm:{fmt}}'
-
-    else:
-        logging.error('invalid COORDINATE_UNITS input. Exiting.')
-        sys.exit(1)
+    return x, y, z, timestamp
 
 
-    logging.info(f"Data for address: {addr}")
-    logging.info(f"Packet type: {packet_type}")
-    logging.info(f"data_code: {data_code}")
-    logging.info(f"data_size: {data_size}")
-    logging.info(f"timestamp: {timestamp}")
-    logging.info(f"X={x}, Y={y}, Z={z}")
-
-    return x, y, z
-
-
-def get_hedge_logfile():
+def get_hedge_logfile(dir):
     """Find the log file by selecting the file with the most recent modification time in the logfile directory.
     If no file found, return None."""
-    logfiles = Path(LOG_FILE_DIRECTORY).glob('*csv')
+    logfiles = Path(dir).glob('*csv')
     max_m_time = -1
     current_log = None
     for lf in logfiles:
@@ -122,63 +158,57 @@ def parse_log_file_fields(fields):
     addr = int(fields[3])
     x_m, y_m, z_m = float(fields[4]), float(fields[5]), float(fields[6])
     in_exclusion_zone = True if int(fields[-4]) else False
-    logging.info(f"Data for address: {addr}")
-    logging.info(f"timestamp: {unix_time}")
-    logging.info(f"X={x_m}, Y={y_m}, Z={z_m}")
     return x_m, y_m, z_m, unix_time, in_exclusion_zone
     
 
-def get_hedge_position_from_log(logfile):
-    # dictionary of hedge addr and x,y,z (as tuple) position
+def get_hedge_position_from_log(logfile, addrs, units='feet.inches', precision=0):
     positions = {}
     in_exclusion_zone = False
-    for addr in HEDGE_ADDRS:
+    for addr in addrs:
         log_data = get_last_logfile_line(logfile, addr)
         x, y, z, unix_time, in_ez = parse_log_file_fields(log_data)
         positions[addr] = (x,y,z)
         if in_ez:
             in_exclusion_zone = True  # if any hedge is in an exclusion zone, we return True 
 
-    if len(HEDGE_ADDRS) == 1:
-        x,y,z = positions[HEDGE_ADDRS[0]]
+    if len(addrs) == 1:
+        x,y,z = positions[addrs[0]]
     else:
         # Get average of all hedges
-        x = sum([positions[addr][0] for addr in HEDGE_ADDRS]) / len(HEDGE_ADDRS)
-        y = sum([positions[addr][1] for addr in HEDGE_ADDRS]) / len(HEDGE_ADDRS)
-        z = sum([positions[addr][2] for addr in HEDGE_ADDRS]) / len(HEDGE_ADDRS)
+        x = sum([positions[addr][0] for addr in addrs]) / len(addrs)
+        y = sum([positions[addr][1] for addr in addrs]) / len(addrs)
+        z = sum([positions[addr][2] for addr in addrs]) / len(addrs)
 
     # X,Y,Z coordinates from logfile are in meters
-    fmt = f' .{COORDINATE_PRECISION}f'
-    if COORDINATE_UNITS == 'feet.inches':
-        x_str = f"""{x / 0.0254 // 12}\' {x / 0.0254 % 12:{fmt}}\""""
-        y_str = f"""{y / 0.0254 // 12}\' {y / 0.0254 % 12:{fmt}}\""""
-        z_str = f"""{z / 0.0254 // 12}\' {z / 0.0254 % 12:{fmt}}\""""
+    fmt = f' .{precision}f'
+    if units == 'feet.inches':
+        x_str = f"""{int(x / 0.0254 / 12): d}\'{abs(x) / 0.0254 % 12:{fmt}}\""""
+        y_str = f"""{int(y / 0.0254 / 12): d}\'{abs(y) / 0.0254 % 12:{fmt}}\""""
+        z_str = f"""{int(z / 0.0254 / 12): d}\'{abs(z) / 0.0254 % 12:{fmt}}\""""
     
-    elif COORDINATE_UNITS == 'inches':
+    elif units == 'inches':
         x_str = f'{x / 0.0254:{fmt}} inches'
         y_str = f'{y / 0.0254:{fmt}} inches'
         z_str = f'{z / 0.0254:{fmt}} inches'
     
-    elif COORDINATE_UNITS == 'mm':
-        x_str = f'{x/1000.:{fmt}} mm'
-        y_str = f'{y/1000.:{fmt}} mm'
-        z_str = f'{z/1000.:{fmt}} mm'
+    elif units == 'mm':
+        x_str = f'{x*1000.:{fmt}} mm'
+        y_str = f'{y*1000.:{fmt}} mm'
+        z_str = f'{z*1000.:{fmt}} mm'
 
-    elif COORDINATE_UNITS == 'm':
+    elif units == 'm':
         x_str = f'{x:{fmt}} m'
         y_str = f'{y:{fmt}} m'
         z_str = f'{z:{fmt}} m'
 
     else:
-        logging.error('invalid COORDINATE_UNITS input. Exiting.')
+        logging.error('invalid units input. Exiting.')
         sys.exit(1)
     
-    return x_str, y_str, z_str, in_exclusion_zone
+    return x_str, y_str, z_str, unix_time, in_exclusion_zone
 
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-    sg.theme('DarkBlue13')
+def create_main_window(settings):
+    sg.theme(settings['color_theme'])
     layout = [
                 [sg.Image('assets/RVB_FRAMATOME_HD_15pct.png')],
                 [sg.Text('Crane Hook Tracker', justification='center', font=('Work Sans', 14))],
@@ -189,38 +219,65 @@ def main():
                 [sg.Text(justification='center', font=('Work Sans', 20), key='-ZPOS-')],
                 [sg.Text('_'*64)],
                 [sg.Text(' '*40)],
-                [sg.Text('   Status: ', size=(11,1), font=('Work Sans', 20)), 
-                 sg.Text('Acquiring Location', size=(30,1), font=('Work Sans', 20), justification='left', text_color='yellow', key='-MSG-')],
+                [sg.Text('  Status:  ', size=(11,1), font=('Work Sans', 20)), 
+                 sg.Text('Acquiring Location', size=(34,2), font=('Work Sans', 20), justification='left', text_color='yellow', key='-MSG-')],
                 [sg.Text(' '*40)],
                 [sg.Text(' '*40)],
                 [sg.Button('Exit', font=('Work Sans', 12)), sg.Button('Settings', font=('Work Sans', 12))],
              ]
-    
-    window = sg.Window('Hook Tracker', 
-                        layout=layout, 
-                        resizable=False,
-                        icon='assets/favicon.ico'
-                       )
-    in_exclusion_zone = False
-    msg_on = True  # funky way to make message flash 
-    while True:
-        event, values = window.read(timeout=UPDATE_FREQUENCY)
-        logging.debug((event, values))
+    return sg.Window('Hook Tracker', layout=layout, resizable=False, icon='assets/favicon.ico')
 
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    window, settings = None, load_settings(SETTINGS_FILE, DEFAULT_SETTINGS) 
+    
+    in_exclusion_zone = False
+    msg_on = True  # funky way to make EXCL. ZONE message flash 
+
+    while True:
+        if window is None:
+            window = create_main_window(settings)
+
+        event, values = window.read(timeout=int(settings['update_freq']))
         if event in (sg.WIN_CLOSED,  'Exit'):
             break
+        if event == 'Settings':
+            event, values = create_settings_window(settings).read(close=True)
+            if event == 'Save':
+                window.close()
+                window = None
+                save_settings(SETTINGS_FILE, settings, values)
+            elif event == 'Restore Defaults':
+                SETTINGS_FILE.unlink(missing_ok=True)
+                window.close()
+                window = None
+                settings = load_settings(SETTINGS_FILE, DEFAULT_SETTINGS) 
+            continue
 
-        hedge_log = get_hedge_logfile()
-        x,y,z,in_ez = get_hedge_position_from_log(hedge_log)
+        hedge_log = get_hedge_logfile(settings['log_dir'])
+        hedge_addrs = [int(x) for x in settings['hedge_addrs'].strip().split(',') if x]
+        logging.info(f"Tracking hedge addresses: {hedge_addrs}")
+        x,y,z,t,in_ez = get_hedge_position_from_log(hedge_log, 
+                                                    hedge_addrs, 
+                                                    units=settings['units'],
+                                                    precision=int(settings['precision']),
+                                                    )
 
-        if in_ez:
+        # Time in log file appears to depend on locality, not UTC.  time.time() returns unix time in UTC
+        # so we have to subtract the offset (time.timezone).
+        sys_log_time_delta = time.time() - time.timezone - t/1000.
+        logging.debug(f'Time delta:  {sys_log_time_delta} sec.')
+        if sys_log_time_delta > float(settings['allowed_system_vs_log_time_delta'])/1000.:
+            logging.warning(f'Time difference ({sys_log_time_delta}) between logfile and system time exceeds threshold.')
+            status_text = "Log data stale.\nPosition may be inaccurate."
+            status_text_color = 'yellow'
+        elif in_ez:
             logging.warning("Hedge in an exclusion zone!!")
             status_text = 'EXCLUSION ZONE' if msg_on else '' 
             msg_on = False if msg_on else True
             status_text_color = 'red'
             winsound.PlaySound("assets/alarm2.wav", winsound.SND_ASYNC | winsound.SND_LOOP)
-            window['-MSG-'].update('INSIDE EXCLUSION ZONE')
-            window['-MSG-'].update(text_color='red')
         else:
             status_text = 'OK'
             status_text_color = 'green'
@@ -231,6 +288,7 @@ def main():
         window['-XPOS-'].update(f'X: {x}')
         window['-YPOS-'].update(f'Y: {y}')
         window['-ZPOS-'].update(f'Z: {z}')
+
     window.close()
 
 if __name__ == '__main__':
